55053745c60196cf3242006a71f07f6661ce2ff7
ragipbaylar <ragipbaylar86@gmail.com>
2023-07-13 15:58:18 -0400

    Day 09

---
+++ src/Day09/_05_ActionsHoverOver.java
@@ -0,0 +1,36 @@
+package Day09;
+
+import Utilities.BaseDriver;
+import Utilities.MyMethods;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
+
+public class _05_ActionsHoverOver extends BaseDriver {
+
+    @Test
+    public void test1(){
+
+        driver.get("https://www.ebay.com/");
+
+        WebElement electronics = driver.findElement(By.linkText("Electronics"));
+
+        Actions actions = new Actions(driver);
+
+        Action action = actions.moveToElement(electronics).build();
+        action.perform();
+
+        MyMethods.myWait(3);
+        WebElement headPhones = driver.findElement(By.linkText("Headphones"));
+        headPhones.click();
+
+        String url = driver.getCurrentUrl();
+
+        Assert.assertTrue("Test Failed",url.contains("Headphones"));
+
+        waitAndQuit();
+    }
+}
